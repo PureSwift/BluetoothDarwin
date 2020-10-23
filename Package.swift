c088b353cb1ecc71d38210d7db6ff2d5cd1fd4b5
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-10-22 20:27:36 -0500

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -1,19 +1,23 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.1
 import PackageDescription
 
-_ = Package(name: "BluetoothDarwin",
-            products: [
-                .library(
-                    name: "BluetoothDarwin",
-                    targets: ["BluetoothDarwin"]
-                )
-            ],
-            dependencies: [
-                .package(url: "https://github.com/PureSwift/Bluetooth.git", .branch("master"))
-            ],
-            targets: [
-                .target(name: "BluetoothDarwin", dependencies: ["Bluetooth", "CBluetoothDarwin"]),
-                .target(name: "CBluetoothDarwin"),
-                .testTarget(name: "BluetoothDarwinTests", dependencies: ["BluetoothDarwin"])
-            ],
-            swiftLanguageVersions: [4])
+let package = Package(
+    name: "BluetoothDarwin",
+    products: [
+        .library(
+            name: "BluetoothDarwin",
+            targets: ["BluetoothDarwin"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/PureSwift/Bluetooth.git",
+            .upToNextMinor(from: "4.2.0")
+        )
+    ],
+    targets: [
+        .target(name: "BluetoothDarwin", dependencies: ["Bluetooth", "CBluetoothDarwin"]),
+        .target(name: "CBluetoothDarwin"),
+        .testTarget(name: "BluetoothDarwinTests", dependencies: ["BluetoothDarwin"])
+    ]
+)
